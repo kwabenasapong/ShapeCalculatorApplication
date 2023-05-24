0032703f1c40fb66544c2544af5a00a69fbdb2a8
Kwabena Boadi Sapong <kwabenasapong@users.noreply.github.com>
2023-05-24 07:35:03 +0000

    Add project files.

---
+++ ShapeCalculatorApplication/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ShapeCalculatorApplication;
+
+ShapeCalculator circle = new ShapeCalculator();
+circle.name = "Circle";
+Console.WriteLine(circle.name);
+
+Console.ReadLine();
